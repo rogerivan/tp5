94b8f739eb327853f77ee86bd34080e5f2896714
Martin Romero <meromero.012@gmail.com>
2013-07-17 00:27:01 -0300

    Agregue que para descuento de menores de doce la fecha en la que concurre el mayor al festival sea la misma que la del menor de 12

--- TP_5/src/descuentos/DescuentoParaMenorDeDoce.groovy
+++ TP_5/src/descuentos/DescuentoParaMenorDeDoce.groovy
@@ -6,13 +6,13 @@ class DescuentoParaMenorDeDoce extends Descuento{
 	@Override
 	def getDescuento(compra){
 		compra.entradasCompradas.each{
-			if ( it.espectador.edad < 12 && this.hayAlgunEspectadorMayor(compra.entradasCompradas) )
+			if ( it.espectador.edad < 12 && this.hayAlgunEspectadorMayor(compra.entradasCompradas, it.noches) )
 				it.descuentosAcumulados += calcularDescuento(it.butacas.first())
 		}
 	}
 	
-	def hayAlgunEspectadorMayor(entradasCompradas){
-		entradasCompradas.any{it.espectador.edad > 18}
+	def hayAlgunEspectadorMayor(entradasCompradas, noches){
+		entradasCompradas.any{it.espectador.edad > 18 && it.noches == noches}
 	}
 	
 	def calcularDescuento(butaca){
