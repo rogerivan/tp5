187827d599cc2847fbbc4abe067101dfdf1c98da
Franco Leonardo Bulgarelli <franco@zauberlabs.com>
2013-07-19 10:24:46 -0300

    Sobre uso de enums

--- TP_5/src/descuentos/DescuentoParaDama.groovy
+++ TP_5/src/descuentos/DescuentoParaDama.groovy
@@ -13,6 +13,12 @@ class DescuentoParaDama extends Descuento{
 	@Override
 	def getDescuento(compra){
 		compra.entradasCompradas.each{
+			//FIXME no es una buena idea modelar valores enumerados que no tengan comportamiento
+			//de dominio usando strings o números, porque es fácil equivocarse al tipear y que nada funcione
+			//o intentar comparar contra valores que no pertenecen a la enumeración (Esto último es más util
+			//en lenguajes con tipado estático)
+			//Recomiendo para tales casos usar enums. Por ejemplo, podrían 
+			//Definir un enum Sexo con valores MASCULINO, FEMENINO y cualquier otra cosa que se les ocurra :P 
 			if( this.porcentajeEntradasCompradasPorDamas() <= 0.2 && it.espectador.sexo == "Femenino"){
 				this.entradasCompradasPorDamas ++
 				it.descuentosAcumulados += calcularDescuento(it.butacas.first())
